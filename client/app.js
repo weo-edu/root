b480b88bcd7c1d9b99ced01968433cb00e3bc796
Andrew <andrew@stopgapdesign.com>
2012-07-07 10:46:31 +0000

    redid some of the window/subapp stuff a bit

--- client/app.js
+++ client/app.js
@@ -1,10 +1,16 @@
-var startupApps = [{name: 'dock', type: 'dock always_on_top'}, {name: 'decks', type: 'primary'}];
+var startupApps = [{name: 'dock', type: 'dock'}, {name: 'decks', type: 'primary'}];
 
 
 Meteor.startup(function(){
   _.each(startupApps, function(app, idx){
     Desktop.spawn(app.name, app.type).foreground();
   });
+
+  Desktop.on('foreground', function(pane){
+  	console.log('new foreground window', pane);
+  });
+
+  Desktop.foreground().background().foreground();
 });
 
 Template.hello.greeting = function () {

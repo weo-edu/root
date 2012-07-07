186d0b0969441ff7aced981cfea97df5a27d8927
Andrew <andrew@stopgapdesign.com>
2012-07-07 01:26:34 +0000

    updated root to use desktop package

---
+++ client/app.js
@@ -0,0 +1,21 @@
+  var desktop = new Desktop;
+
+var desktop;
+var startupApps = [{name: 'decks', type: 'primary'}, {name: 'decks', type: 'dock'}];
+
+Meteor.startup(function(){
+  desktop = new Desktop;
+  _.each(startupApps, function(app, idx){
+    desktop.spawn(app.name, app.type).foreground();
+  });
+});
+
+Template.hello.greeting = function () {
+  return "Welcome to root.";
+};
+
+Template.hello.events = {
+  'click input' : function (){      
+    desktop.spawn('decks');
+  }
+};
--- root.js
+++ root.js
@@ -1,19 +1,4 @@
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to root.";
-  };
-
-  var decks;
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if(decks)
-        decks.destroy();
-      
-      process.fork_child('/app!decks')
-
-    }
-  };
 }
 
 if (Meteor.is_server) {

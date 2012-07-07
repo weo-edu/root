d702e83c6d583e7f98dfb9283299a20e302b382f
joshrtay <joshrtay@gmail.com>
2012-07-07 10:54:45 -0700

    added purl lib

--- root.js
+++ root.js
@@ -1,19 +1,13 @@
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to root.";
-  };
+  console.log('fork');
+  Meteor.defer(function() {
+    purl.initRoot();
+    purl.proxy(process.fork_child('/app!propurl'));
+    purl.on('app:change',function(url) {
+    	alert('tried to change path to ' + url.href);
+    })
 
-  var decks;
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if(decks)
-        decks.destroy();
-      
-      process.fork_child('/app!decks')
-
-    }
-  };
+  });
 }
 
 if (Meteor.is_server) {

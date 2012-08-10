048910815dd247e7e5f64d76488c0e6287ecea1b
joshrtay <joshrtay@gmail.com>
2012-08-09 23:01:23 -0700

    root changes

--- client/app.js
+++ client/app.js
@@ -1,8 +1,26 @@
-var startupApps = [{name: 'dock', type: 'dock'}, {name: 'purl-test', type: 'primary'}];
+Launcher = {
+  startupApps: [{name: 'dock', type: 'dock'}, {name: 'purl-test', type: 'primary'}],
+  start: function() {
+    _.each(this.startupApps, run);
+  },
+  launch: function (url){
+    var name = utils.getAppFromPath(url);
+    var path = url.replace('/' + name, '');
+    this.run({name: name, type: 'primary', path: path});
+  },
+  run: function(app) {
+    var pane = Desktop.spawn(app.name, app.type, app.path).foreground();
+    purl(pane.process);
+    pane.process.on('purl:app', launch);
+    pane.process.on('purl:host', function(url) {
+      window.open(url);
+    });
+  }
+}
 
 route('/sub!*', function(ctx){
   $(function(){
-    launch(ctx.path.replace('sub!', __meteor_runtime_config__.METEOR_SUBAPP_PREFIX));
+    Launcher.launch(ctx.path.replace('sub!', __meteor_runtime_config__.METEOR_SUBAPP_PREFIX));
   });
 
 });
@@ -12,39 +30,8 @@ $(window).resize(function(){
 });
 
 
-function openHost(url){
-  window.open(url);
-}
-
-function launch(url){
-  var name = utils.getAppFromPath(url);
-  var path = url.replace('/' + name, '');
-  run({name: name, type: 'primary', path: path});
-}
-
-function run(app){
-    var pane = Desktop.spawn(app.name, app.type, app.path).foreground();
-    purl(pane.process);
-    pane.process.on('purl:app', launch);
-    pane.process.on('purl:host', openHost);
-}
-
-function runStartups(){
-  _.each(startupApps, run);
-}
-
 Meteor.startup(function(){
- // Meteor.defer(runStartups);
- runStartups();
- route.start();
+  Launcher.start();
+  route.start();
 });
 
-Template.hello.greeting = function () {
-  return "Welcome to root.";
-};
-
-Template.hello.events = {
-  'click input' : function (){      
-    desktop.spawn('decks');
-  }
-};
--- root.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.is_client) {
-
-}
-
-if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}

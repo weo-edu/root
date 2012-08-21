0f8040c80597d9b05e8299f91eb937f00cb1140e
mweint <mweint@gmail.com>
2012-08-20 17:28:22 -0700

    updating

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var startupApps = [{name: 'dock', type: 'dock'}];
+var startupApps = [{name: 'home', type: 'primary'},  {name: 'dock', type: 'dock'}];
 
 route('/sub!*', function(ctx){
   $(function(){

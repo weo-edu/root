0e2df817f7b8d896a51b86dbbcb5146b2c04c127
Andrew Shaffer <andrew@weo.io>
2012-08-22 17:14:36 -0700

    various updates

---
+++ server/startup.js
@@ -0,0 +1,14 @@
+/*
+	Startup contains a hodgepodge of things necessary to ensure
+	the existence of basic database structure and any other preliminary
+	configuration that might be necessary
+*/
+;(function(){
+	Meteor.startup(function(){
+		var actions = ['user_message', 'friend', 'unfriend', 'login', 'logout', 'complete', 'create', 'message', 'pet'];
+
+		_.each(actions, function(action){
+			Meteor.call('registerAction', {name: action});
+		});
+	});
+})();

0a651bbb99385b0abd73b15ef56e9d1e0871fa1d
joshrtay <joshrtay@gmail.com>
2012-09-05 00:17:12 -0700

    launcher file

---
+++ client/launcher.js
@@ -0,0 +1,46 @@
+Launcher = {
+  startupApps: [{name: 'home', type: 'primary'}, {name: 'dock', type: 'dock'}],
+  start: function() {
+    _.each(this.startupApps, this.run.bind(this));
+  },
+  launch: function (url){
+    var name = utils.getAppFromPath(url);
+    var path = url.replace('/' + name, '');
+    this.run({name: name, type: 'primary', path: path});
+  },
+  run: function(app, forceFore) {
+    var self = this;
+    var pane = Desktop.spawn(app.name, app.type, app.path, function(process) {
+      purl(process);
+      process.on('purl:app', self.launch.bind(self));
+      process.on('purl:host', function(url) {
+        window.open(url);
+      });
+    });
+
+    if(!Desktop.foreground(pane.type) || forceFore)
+      pane.foreground();
+
+    
+  }
+}
+
+route('/sub!*', function(ctx, next){
+  $(function(){
+    Launcher.launch(ctx.path.replace('sub!', __meteor_runtime_config__.METEOR_SUBAPP_PREFIX));
+    next();
+  });
+});
+
+route('*', function(ctx) {
+  Launcher.start();
+});
+
+$(window).resize(function(){
+  Desktop.layout();
+});
+
+Meteor.startup(function(){
+  route.start();
+});
+
